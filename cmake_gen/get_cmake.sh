f2ac6c80ec9cadfb273c23fe9618cfc0f7813350
1ndev-ui <1ndevelopment@protonmail.com>
2024-06-22 17:34:45 -0700

    CMake version checking

--- cmake_gen/get_cmake.sh
+++ cmake_gen/get_cmake.sh
@@ -1,20 +1,73 @@
 #!/usr/bin/env bash
 
-cmake_build() {
-      cmake_version="$1"
-      jobs="$2"
+# Build chosen cmake version from source
+build_cmake() {
+    cmake_version="$1"
+    jobs="$2"
 
-      if [ -z "$cmake_version" ]; then
-            cmake_version=$(curl -s 'https://cmake.org/files/LatestRelease/cmake-latest-files-v1.json' | jq -r '.version.string')
-      fi
+    if [ -z "$cmake_version" ]; then
+        cmake_version=$(curl -s 'https://cmake.org/files/LatestRelease/cmake-latest-files-v1.json' | jq -r '.version.string')
+    fi
 
-      if [ -z "$jobs" ]; then
-            jobs=$(nproc)
-      fi
+    if [ -z "$jobs" ]; then
+        jobs=$(nproc)
+    fi
 
-      curl -OL https://github.com/Kitware/CMake/releases/download/v$cmake_version/cmake-$cmake_version.tar.gz
-      tar -xzf cmake-$cmake_version.tar.gz
-      cd cmake-$cmake_version
-      ./bootstrap -- -DCMAKE_BUILD_TYPE:STRING=Release
-      make -j$jobs && make install
+    curl -OL https://github.com/Kitware/CMake/releases/download/v$cmake_version/cmake-$cmake_version.tar.gz
+    tar -xzf cmake-$cmake_version.tar.gz
+    cd cmake-$cmake_version
+    ./bootstrap -- -DCMAKE_BUILD_TYPE:STRING=Release
+    make -j$jobs && make install
 }
+
+compare() {
+# Function to compare version numbers
+    version_compare() {
+        if [[ $1 == $2 ]]; then
+            return 0
+        fi
+        local IFS=.
+        local i ver1=($1) ver2=($2)
+        for ((i=${#ver1[@]}; i<${#ver2[@]}; i++)); do
+            ver1[i]=0
+        done
+        for ((i=0; i<${#ver1[@]}; i++)); do
+            if [[ -z ${ver2[i]} ]]; then
+                ver2[i]=0
+            fi
+            if ((10#${ver1[i]} > 10#${ver2[i]})); then
+                return 1
+            fi
+            if ((10#${ver1[i]} < 10#${ver2[i]})); then
+                return 2
+            fi
+        done
+        return 0
+    }
+
+# Required CMake version
+required_version="$1"
+
+# Get installed CMake version
+installed_version=$(cmake --version | head -n1 | awk '{print $3}')
+
+# Compare versions
+version_compare "$installed_version" "$required_version"
+comparison=$?
+
+if [ $comparison -eq 0 ] || [ $comparison -eq 1 ]; then
+    echo "CMake version $installed_version is installed and meets the requirement (>= $required_version)."
+else
+    echo "CMake version $installed_version is installed but does not meet the requirement (>= $required_version)."
+    exit 1
+fi
+}
+
+# Check if CMake is installed
+if ! command -v cmake &> /dev/null; then
+    echo "CMake is not installed."
+    build_cmake $1 $2
+    exit 1
+fi
+
+compare $1
